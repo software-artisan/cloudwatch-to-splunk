9457b42dd0bfcfdecf8b450b10b2905fe8523b06
Jagane Sundar <jagane@infinstor.com>
2022-11-22 18:05:08 -0800

    catch exceptions

--- node1/get_logs.py
+++ node1/get_logs.py
@@ -9,16 +9,18 @@ import tzlocal
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--access_key_id', help='aws access key id', required=True)
-parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
+try:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--access_key_id', help='aws access key id', required=True)
+    parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
-paginator = client.get_paginator('describe_log_groups')
-for pg in paginator.paginate():
-    for group in pg['logGroups']:
-        print(group['logGroupName'])
-
+    client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
+    paginator = client.get_paginator('describe_log_groups')
+    for pg in paginator.paginate():
+        for group in pg['logGroups']:
+            print(group['logGroupName'])
+except Exception as e1:
+    print('Caught ' + str(e1), flush=True)
 os._exit(os.EX_OK)
