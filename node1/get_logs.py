ac2cf0c4f138942675e7cc806bf11cf55fb7310a
Jagane Sundar <jagane@infinstor.com>
2022-11-22 17:34:31 -0800

    more

--- node1/get_logs.py
+++ node1/get_logs.py
@@ -6,9 +6,19 @@ import os
 import sys
 import datetime
 import tzlocal
+import argparse
 
 
-for a in sys.argv:
-    print(str(a), flush=True)
+parser = argparse.ArgumentParser()
+parser.add_argument('--access_key_id', help='aws access key id', required=True)
+parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
+
+args = parser.parse_args()
+
+client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
+paginator = client.get_paginator('describe_log_groups')
+for pg in paginator.paginate():
+    for group in pg['logGroups']:
+        print(group['logGroupName'])
 
 os._exit(os.EX_OK)
