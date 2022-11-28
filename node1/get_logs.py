d13171031c95de1dd1969c628516a18f901f25d0
Jagane Sundar <jagane@infinstor.com>
2022-11-28 14:07:24 -0800

    more

--- node1/get_logs.py
+++ node1/get_logs.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 
 try:
-    from time import time
-    import boto3
-    import os
-    import sys
-    import datetime
-    import tzlocal
-    import argparse
+  from time import time
+  import boto3
+  import os
+  import sys
+  import datetime
+  import tzlocal
+  import argparse
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--access_key_id', help='aws access key id', required=True)
-    parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--access_key_id', help='aws access key id', required=True)
+  parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
 
-    args = parser.parse_args()
+  args = parser.parse_args()
 
-    client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
-    paginator = client.get_paginator('describe_log_groups')
-    page_iterator = paginator.paginate(includeLinkedAccounts=False)
+  client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
+  paginator = client.get_paginator('describe_log_groups')
+  nextToken = None
+  while True:
+    page_iterator = paginator.paginate(limit=1000, nextToken=nextToken)
     print('aaaaaaaaaaaaaaaa', flush=True)
     for pg in page_iterator:
-        print('pg=' + str(pg), flush=True)
-        for group in pg['logGroups']:
-            print(group['logGroupName'])
+      print('pg=' + str(pg), flush=True)
+      for group in pg['logGroups']:
+          print(group['logGroupName'], flush=True)
+    if 'NextToken' in page_iterator:
+      nextToken = page_iterator['NextToken']
+    else:
+      break
 except Exception as e1:
-    print('Caught ' + str(e1), flush=True)
+  print('Caught ' + str(e1), flush=True)
 os._exit(os.EX_OK)
