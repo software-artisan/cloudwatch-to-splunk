086cda1a1d764e3579a61f2bd56704f6c2d2225f
Jagane Sundar <jagane@infinstor.com>
2022-11-28 11:27:47 -0800

    more

--- node1/get_logs.py
+++ node1/get_logs.py
@@ -8,18 +8,7 @@ try:
     import datetime
     import tzlocal
     import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--access_key_id', help='aws access key id', required=True)
-    parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
-
-    args = parser.parse_args()
-
-    client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
-    paginator = client.get_paginator('describe_log_groups')
-    for pg in paginator.paginate():
-        for group in pg['logGroups']:
-            print(group['logGroupName'])
+    print('Hello World', flush=True)
 except Exception as e1:
     print('Caught ' + str(e1), flush=True)
 os._exit(os.EX_OK)
