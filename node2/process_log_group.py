c9d7d0e5e943ad2d9979d8359b9131adab64280e
Jagane Sundar <jagane@infinstor.com>
2022-11-28 19:10:03 -0800

    ndoe2

---
+++ node2/process_log_group.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+try:
+  from time import time
+  import boto3
+  import os
+  import sys
+  import datetime
+  import tzlocal
+  import argparse
+  from concurrent_plugin import concurrent_core
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--access_key_id', help='aws access key id', required=True)
+  parser.add_argument('--secret_access_key', help='aws secret access key', required=True)
+
+  args = parser.parse_args()
+
+  client = boto3.client('logs', region_name='us-east-1', aws_access_key_id=args.access_key_id, aws_secret_access_key=args.secret_access_key)
+
+  df = concurrent_core.list(None)
+  print('Column Names:', flush=True)
+  cn = df.columns.values.tolist()
+  print(str(cn))
+  print('------------------------------ Obtained input info ----------------', flush=True)
+  for ind, row in df.iterrows():
+      print("Input row=" + str(row), flush=True)
+  print('------------------------------ Finished dump of input info ----------------', flush=True)
+  os._exit(os.EX_OK)
+
+
+  paginator = client.get_paginator('describe_log_groups')
+  nextToken = None
+  ind = 0
+  while True:
+    if nextToken:
+      page_iterator = paginator.paginate(limit=50, nextToken=nextToken)
+    else:
+      page_iterator = paginator.paginate(limit=50)
+    for pg in page_iterator:
+      print('pg=' + str(pg), flush=True)
+      for group in pg['logGroups']:
+        print(group['logGroupName'], flush=True)
+        fn = '/tmp/emptyfile-' + str(ind)
+        open(fn, 'a').close()
+        ind = ind + 1
+        concurrent_core.concurrent_log_artifact(fn, "dummy", LogGroupName=group['logGroupName'])
+    if 'NextToken' in page_iterator:
+      nextToken = page_iterator['NextToken']
+    else:
+      break
+except Exception as e1:
+  print('Caught ' + str(e1), flush=True)
+os._exit(os.EX_OK)
