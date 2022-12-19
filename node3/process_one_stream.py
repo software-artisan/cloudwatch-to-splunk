19cc270ff81bf4b9a1bdf1257c38125fb0216e6c
Jagane Sundar <jagane@infinstor.com>
2022-12-18 20:42:18 -0800

    more

--- node3/process_one_stream.py
+++ node3/process_one_stream.py
@@ -15,16 +15,15 @@ def aws_cloudwatch_url(region, log_group, log_stream, dt):
         nested_quote(log_stream) + quote_once('?start=') + nested_quote(str(dt)),
     ])
 
-def add_log_line(dt, person, log_line, all_messages, log_group, log_stream, region):
+def add_log_line(dt, person, all_messages, log_group, log_stream, region):
   cw_url = aws_cloudwatch_url(region, log_group, log_stream, dt)
   print('CloudWatch URL is: ' + cw_url)
   if person in all_messages:
     all_messages[person].append(cw_url)
   else:
-    all_messages[person] = [log_line]
+    all_messages[person] = [cw_url]
 
-def process_one_log_stream_inner(client, ner, fp, group_name, stream_name, region, all_messages):
-    print(f'Dumping log stream inner: {fp.name}')
+def process_one_log_stream(client, ner, group_name, stream_name, region, all_messages):
     nt = None
     while (True):
         print('.... nt=' + str(nt))
@@ -37,7 +36,6 @@ def process_one_log_stream_inner(client, ner, fp, group_name, stream_name, regio
         for event in events:
             dt = datetime.datetime.fromtimestamp(event['timestamp']/1000, datetime.timezone.utc)
             msg = event['message']
-            # fp.write(str(dt) + ' : ' + msg)
             s = ner(msg)
             orgs = []
             persons = []
@@ -48,7 +46,7 @@ def process_one_log_stream_inner(client, ner, fp, group_name, stream_name, regio
                 orgs.append(entry['word'])
               elif entry['entity_group'] == 'PER':
                 persons.append(entry['word'])
-                add_log_line(dt, entry['word'], msg, all_messages, group_name, stream_name, region)
+                add_log_line(dt, entry['word'], all_messages, group_name, stream_name, region)
               elif entry['entity_group'] == 'MISC':
                 misc.append(entry['word'])
             print(str(dt) + ": orgs=" + str(orgs) + ", persons=" + str(persons) + ", misc=" + str(misc) + " : " + msg)
@@ -59,30 +57,6 @@ def process_one_log_stream_inner(client, ner, fp, group_name, stream_name, regio
             nt = resp['nextForwardToken']
         else:
           break
-
-def process_one_log_stream(client, ner, stream_name, log_group_name, region, creation_time, all_messages):
-    datetime_local:datetime.datetime = datetime.datetime.fromtimestamp(creation_time/1000, tz=tzlocal.get_localzone())  #tz=datetime.timezone.utc
-    fname = (str(datetime_local) + '_' + stream_name ).replace("/","_").replace(" ","_")
-    print(f"Dumping log stream {stream_name} to file /tmp/{fname}", flush=True)
-    with open('/tmp/' + fname, 'w') as fp:
-        process_one_log_stream_inner(client, ner, fp, log_group_name, stream_name, region, all_messages)
-
-def process_one_log_group(client, ner, log_group_name, region, all_messages):
-  nextToken = None
-  while True:
-    if nextToken:
-      rv = client.describe_log_streams(logGroupName=log_group_name, orderBy='LastEventTime', descending=True, limit=50, nextToken=nextToken)
-    else:
-      rv = client.describe_log_streams(logGroupName=log_group_name, orderBy='LastEventTime', descending=True, limit=50)
-    log_streams = rv['logStreams']
-    for one_stream in log_streams:
-      process_one_log_stream(client, ner, one_stream['logStreamName'], log_group_name, region, one_stream['creationTime'], all_messages)
-      break
-    break
-    if ('nextToken' in rv):
-      nextToken = rv['nextToken']
-    else:
-      break
 
 try:
   from time import time
@@ -131,7 +105,14 @@ try:
   all_messages = {}
   for ind, row in df.iterrows():
     print("Input row=" + str(row), flush=True)
+    process_one_log_stream(client, ner, row['LogGroupName'], row['LogStreamName'], row['region'], all_messages)
   print('------------------------------ Finished Input ----------------', flush=True)
+
+  print('------------------------------ Start Logging Artifact ----------------', flush=True)
+  with open("/tmp/output.json", 'w') as fp:
+    json.dump(all_messages, fp)
+  concurrent_core.concurrent_log_artifact("/tmp/output.json", "")
+  print('------------------------------ End Logging Artifact ----------------', flush=True)
   os._exit(os.EX_OK)
 except Exception as e1:
   print("Caught " + str(e1), flush=True)
